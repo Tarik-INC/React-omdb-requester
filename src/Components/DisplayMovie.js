bcb45d0c46fa07cc445716ae2378fb243f07906f
Tarik <tarikesmin@gmail.com>
2018-11-19 16:11:36 -0200

    Movies are now sorted

--- src/Components/DisplayMovie.js
+++ src/Components/DisplayMovie.js
@@ -4,16 +4,15 @@ import './DisplayMovie.css';
 const DisplayMovie = (props) => {
 
 
-    if (props.movies.length > 1) {
-        props.movies.sort((a, b) => (a.Title > b.Ttile) ? 1 : 0)
-    }
+    
+        props.movies.sort((a, b) => (a.Year >= b.Year) ? 1 : (a.Year < b.Year) ? -1: 0)
+    
 
     const movies = props.movies.map((movie, index) => {
         return (
             <div key={index} className='movie_container'>
                 <div className='movie_elements'>
                     <h1 > {movie.Title} </h1>
-                        <p> {movie.Plot}</p>
                         <p> Year:  {movie.Year}</p>
                     <img src={movie.Poster} alt='Movie representational poster' />
                 </div>
